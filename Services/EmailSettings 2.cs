40273e6903a97d49c3365750c9c51747f7b637f2
anabelenr8 <ana.romero0501@gmail.com>
2025-05-05 09:51:19 +0100

    fixed bad pull

--- Services/EmailSettings 2.cs
+++
@@ -1,7 +0,0 @@
-public class EmailSettings
-{
-    public required string SmtpServer { get; set; }
-    public int SmtpPort { get; set; }
-    public required string SmtpUsername { get; set; }
-    public required string SmtpPassword { get; set; }
-}
